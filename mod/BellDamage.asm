9255ff51daeefc873cbbcec2f01c3db67da0fdf1
Modoh <modohhh@gmail.com>
2024-07-30 05:09:41 -0400

    reverted combattweaks bell base damage increase

--- mod/BellDamage.asm
+++ mod/BellDamage.asm
@@ -7,30 +7,31 @@ incsrc "utility/damageutil.asm"
 %subdef(BellDamage)
 
 if !_CombatTweaks
-;Tweaks:
-;Increases bell damage from 50-100% base damage to 75-125%
-;Goal is to make bells usable and have a purpose
 ;Overrides Attack Power with Param 1 (for harp magic mod, but applies to bells too if data is changed)
 	LDA Param1		;*param 1						
 	BNE .SkipBP		;*if set, use Param 1 instead of BP			
-	JSR LoadAttackPower	;*load Attack Power including Power Drinks 		
-.SkipBP
-	TAX
-	STX $0E		;AP
-	LDX #$0000
-	JSR Random_X_A	;0..AP
-	REP #$21	
-	ADC $0E		;AP..AP*2 (100..200%)			
-	LSR $0E		;AP/2 (50%)
-	ADC $0E		;AP+AP/2..AP*2+AP/2	(150%..250%) 	;sometimes adds carry, that's fine 
-	LSR		;AP/2+AP/4..AP+AP/4	(75%..125%)
-	STA Attack
-else
+endif
+
+
+;this code raises bell damage to 75-125%
+;seemed too strong in testing, considering the attack is 100% accurate/unavoidable
+;	TAX
+;	STX $0E		;AP
+;	LDX #$0000
+;	JSR Random_X_A	;0..AP
+;	REP #$21	
+;	ADC $0E		;AP..AP*2 (100..200%)			
+;	LSR $0E		;AP/2 (50%)
+;	ADC $0E		;AP+AP/2..AP*2+AP/2	(150%..250%) 	;sometimes adds carry, that's fine 
+;	LSR		;AP/2+AP/4..AP+AP/4	(75%..125%)
+;	STA Attack
+
+;Standard Bell Damage
 ;Attack = Attack Power / 2 + 0..(Attack Power / 2)
 ;M = Level*Agility/128 + 2 + Level*MagicPower/128
 ;Defense = Target Magic Defense 
 	JSR LoadAttackPower
-	LSR
+.SkipBP	LSR
 	TAX
 	STX $0E		;AP/2
 	LDX #$0000
@@ -38,7 +39,6 @@ else
 	REP #$21
 	ADC $0E		;AP/2 + 0..AP/2 (50%..100%)
 	STA Attack
-endif
 	
 	TDC 		
 	SEP #$20		
